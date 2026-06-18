2aa2a2ad417a7a2f2a9c72bff91b025e4f5ee14d
Afnan Sultan Trino <160451957+Afnan-Sultan-Trino@users.noreply.github.com>
2026-06-19 01:54:37 +0600

    Another Built-in module os,path

---
+++ os.js
@@ -0,0 +1,11 @@
+
+// Built-in Module os,path
+
+//.... os module/ Operating System.....//
+
+const os=require("os")
+console.log(os.userInfo());    //Will give userinfo such as username :"AS", homedir:"C:\\Users\\AS"
+console.log(os.homedir());     // Will give specific info 
+console.log(os.hostname());    // Host name : DESKTOP-UH9KLVQ
+console.log(os.totalmem());   // Total Memory for this laptop
+console.log(os.freemem());    // Free Memory for this laptop
